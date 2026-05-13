6f9b27256a499d500ad6ef7767543516bc90716f
s-irlui <118243008+s-irlui@users.noreply.github.com>
2026-05-13 11:25:07 +0300

    Fetch toys from API and manage state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,51 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
 
-  function handleClick() {
-    setShowForm((showForm) => !showForm);
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((r) => r.json())
+      .then((toyData) => setToys(toyData));
+  }, []);
+
+  // STEP 3: POST - Add a Toy
+  function handleAddToy(newToy) {
+    setToys([...toys, newToy]);
+  }
+
+  // STEP 4: DELETE - Donate a Toy
+  function handleDeleteToy(id) {
+    const updatedToys = toys.filter((toy) => toy.id !== id);
+    setToys(updatedToys);
+  }
+
+  // STEP 5: PATCH - Like a Toy
+  function handleLikeToy(updatedToy) {
+    const updatedToys = toys.map((toy) =>
+      toy.id === updatedToy.id ? updatedToy : toy
+    );
+    setToys(updatedToys);
   }
 
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
-        <button onClick={handleClick}>Add a Toy</button>
+        <button onClick={() => setShowForm(!showForm)}>
+          {showForm ? "Hide Form" : "Add a Toy"}
+        </button>
       </div>
-      <ToyContainer />
+      <ToyContainer 
+        toys={toys} 
+        onDeleteToy={handleDeleteToy} 
+        onLikeToy={handleLikeToy} 
+      />
     </>
   );
 }
